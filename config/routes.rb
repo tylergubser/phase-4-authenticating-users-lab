dca9c746045ce3a59993dd7105476974d0d6510c
tylergubser <tylergubser2200@gmail.com>
2022-04-11 09:55:13 -0500

    commit tree de27309a5ab898f06ca30807b9ff2ec48dfa613c

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   resources :articles, only: [:index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  delete "/logout", to: "sessions#destroy"
+  post "/login", to: "sessions#create"
+  get "/me", to: "users#show"
+
 end
